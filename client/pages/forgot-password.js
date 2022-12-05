bfd49559cc9e91b7ef0efa81c1b47eb965889770
athamaulana096 <athamaulana30@gmail.com>
2022-12-06 02:10:12 +0700

    Mengganti sedikit konfigurasi lupa password

--- client/pages/forgot-password.js
+++ client/pages/forgot-password.js
@@ -1,4 +1,4 @@
-import { useState, useContext, useEffect, use } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { SyncOutlined } from '@ant-design/icons';
@@ -7,33 +7,33 @@ import { Context } from '../context';
 import { useRouter } from 'next/router';
 
 const ForgotPassword = () => {
-  //state
+  // state
   const [email, setEmail] = useState('');
   const [success, setSuccess] = useState(false);
   const [code, setCode] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [loading, setLoading] = useState(false);
 
-  //context
+  // context
   const {
     state: { user },
   } = useContext(Context);
-
-  //router
+  // router
   const router = useRouter();
 
-  //redirect if user login
+  // redirect if user is logged in
   useEffect(() => {
     if (user !== null) router.push('/');
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setLoading(true);
     try {
-      setLoading(true);
       const { data } = await axios.post('/api/forgot-password', { email });
       setSuccess(true);
-      toast('Check your email for secret Code');
+      toast('Check your email for the secret code');
+      setLoading(false);
     } catch (err) {
       setLoading(false);
       toast(err.response.data);
@@ -46,16 +46,16 @@ const ForgotPassword = () => {
     // return;
     try {
       setLoading(true);
-      const { data } = await axios.prototype('/api/reset-password', {
+      const { data } = await axios.post('/api/reset-password', {
         email,
         code,
         newPassword,
       });
+      toast.success('You can use new password');
       setEmail('');
       setCode('');
       setNewPassword('');
       setLoading(false);
-      toast('Great! Now you can login with new Password');
     } catch (err) {
       setLoading(false);
       toast(err.response.data);
@@ -71,14 +71,19 @@ const ForgotPassword = () => {
               <span className="title">Forgot Password</span>
               <form onSubmit={success ? handleResetPassword : handleSubmit}>
                 <div className="input-field">
-                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" required />
-                  {success && (
-                    <>
-                      <input type="text" value={code} onChange={(e) => setCode(e.target.value)} placeholder="Enter secret code" required />
-                      <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="Enter your new password" required />
-                    </>
-                  )}
+                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email" required />{' '}
                 </div>
+                {success && (
+                  <>
+                    <div className="input-field">
+                      <input type="text" value={code} onChange={(e) => setCode(e.target.value)} placeholder="Enter secret code" required />
+                    </div>
+                    <div className="input-field">
+                      <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} placeholder="New Password" required />
+                    </div>
+                  </>
+                )}
+
                 <div className="input-field buttonLogin">
                   <button type="submit" disabled={loading || !email}>
                     {loading ? <SyncOutlined spin /> : 'Submit'}
