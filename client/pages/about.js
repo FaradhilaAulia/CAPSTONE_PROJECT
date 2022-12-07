eb7b47df6bb55ea8de597c2e6b323f48b627c492
athamaulana096 <athamaulana30@gmail.com>
2022-12-07 10:31:34 +0700

    Sedikit perbaikan page About

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,38 +1,40 @@
-const About = () => {
-    return (
-        <>
-            <h1 className="container-fluid p-5 bg-primary text-white text-center">SolusiPintar</h1>
-            <section id="about">
-                <div className="team">
-                    <h3>Our team</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, laborum!</p>
+import Image from 'next/image';
 
-                    <div className="tim">
-                        <div>
-                            <img src="./public/fachriza.jpeg" />
-                            <h6>Fachriza Adrian</h6>
-                            <span> Universitas Sumatera Utara </span>
-                        </div>
-                        <div>
-                            <img src="./public/dhila.jpeg" />
-                            <h6>Faradhila Aulia </h6>
-                            <span> Universitas Sumatera Utara </span>
-                        </div>
-                        <div>
-                            <img src="./public/Atha.jpeg" />
-                            <h6>Atha Maulana</h6>
-                            <span> Universitas Sumatera Utara </span>
-                        </div>
-                        <div>
-                            <img src="./public/najwa.jpeg" />
-                            <h6>Najwa Syiba</h6>
-                            <span> Unversitas Andalas </span>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    );
+const About = () => {
+  return (
+    <>
+      <h1 className="container-fluid p-5 bg-primary text-white text-center">SolusiPintar</h1>
+      <section id="about">
+        <div className="team">
+          <h3>Our team</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, laborum!</p>
+
+          <div className="tim">
+            <div>
+              <Image className="img" src="/profile/fachriza.jpeg" width={200} height={200} />
+              <h6>Fachriza Adrian</h6>
+              <span> Universitas Sumatera Utara </span>
+            </div>
+            <div>
+              <Image className="img" src="/profile/dhila.jpeg" width={200} height={200} />
+              <h6>Faradhila Aulia </h6>
+              <span> Universitas Sumatera Utara </span>
+            </div>
+            <div>
+              <Image className="img" src="/profile/atha.jpeg" width={200} height={200} />
+              <h6>Atha Maulana</h6>
+              <span> Universitas Sumatera Utara </span>
+            </div>
+            <div>
+              <Image className="img" src="/profile/najwa.jpg" width={200} height={200} />
+              <h6>Najwa Syiba</h6>
+              <span> Unversitas Andalas </span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default About;
