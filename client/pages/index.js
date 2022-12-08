6444aedfb6d30804473a88991f84ab7b91d1a1b3
FaradhilaAulia <dhila.tanjung@gmail.com>
2022-12-08 23:32:56 +0700

    menambahkan isi app

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
-import { TrophyFilled, SafetyCertificateFilled, GiftFilled } from '@ant-design/icons';
+import { TrophyFilled, SafetyCertificateFilled, GiftFilled, StarFilled } from '@ant-design/icons';
+import Image from 'next/image';
 const Index = () => {
     return (
         <>
@@ -24,6 +25,104 @@ const Index = () => {
                     </div>
                 </div>
             </section>
+
+            <section id="course">
+                <h1>Lorem Ipsum</h1>
+                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <div class="course-box">
+                    <div class="courses">
+                        <Image className="img" src="/courses/c1.jpg" width={400} height={400} />
+                        <div class="details">
+                            <span>Lorem Ipsum is simply dummy</span>
+                            <h6>Lorem Ipsum</h6>
+                            <div class="star">
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <span>(123)</span>
+                            </div>
+                        </div>
+                        <div class="cost">FREE</div>
+                    </div>
+
+                    <div class="courses">
+                        <Image className="img" src="/courses/c2.jpg" width={400} height={400} />
+                        <div class="details">
+                            <span>Lorem Ipsum is simply dummy</span>
+                            <h6>Lorem Ipsum</h6>
+                            <div class="star">
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <span>(123)</span>
+                            </div>
+                        </div>
+                        <div class="cost">FREE</div>
+                    </div>
+
+                    <div class="courses">
+                        <Image className="img" src="/courses/c3.jpg" width={400} height={400} />
+                        <div class="details">
+                            <span>Lorem Ipsum is simply dummy</span>
+                            <h6>Lorem Ipsum</h6>
+                            <div class="star">
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <span>(123)</span>
+                            </div>
+                        </div>
+                        <div class="cost">FREE</div>
+                    </div>
+
+                    <div class="courses">
+                        <Image className="img" src="/courses/c4.jpg" width={400} height={400} />
+                        <div class="details">
+                            <span>Lorem Ipsum is simply dummy</span>
+                            <h6>Lorem Ipsum</h6>
+                            <div class="star">
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <i><StarFilled /></i>
+                                <span>(123)</span>
+                            </div>
+                        </div>
+                        <div class="cost">FREE</div>
+                    </div>
+                </div>
+            </section>
+
+            <div className="team">
+                <h3>Our team</h3>
+                <p>Inilah kami, dibalik website yang sedang kalian jalankan ada ide dan pembuatan yang dikerjakan oleh kami.</p>
+
+                <div className="tim">
+                    <div>
+                        <Image className="img" src="/profile/fachriza.jpeg" width={200} height={200} />
+                        <h6>Fachriza Adrian</h6>
+                        <span> Universitas Sumatera Utara </span>
+                    </div>
+                    <div>
+                        <Image className="img" src="/profile/dhila.jpeg" width={200} height={200} />
+                        <h6>Faradhila Aulia </h6>
+                        <span> Universitas Sumatera Utara </span>
+                    </div>
+                    <div>
+                        <Image className="img" src="/profile/atha.jpeg" width={200} height={200} />
+                        <h6>Atha Maulana</h6>
+                        <span> Universitas Sumatera Utara </span>
+                    </div>
+                    <div>
+                        <Image className="img" src="/profile/najwa.jpg" width={200} height={200} />
+                        <h6>Najwa Syiba</h6>
+                        <span> Unversitas Andalas </span>
+                    </div>
+                </div>
+            </div>
 
 
         </>
