8ad77622b0917a40703b882e436c1e710f4c95ef
athamaulana096 <athamaulana30@gmail.com>
2022-12-08 19:28:31 +0700

    Membuat form course

--- client/component/nav/UserNav.js
+++ client/component/nav/UserNav.js
@@ -1,21 +1,19 @@
-import Link from "next/link";
+import Link from 'next/link';
+import { useState, useEffect } from 'react';
 
+const UserNav = () => {
+  const [current, setCurrent] = useState('');
 
-const UserNav = ( ) => {
-    const [current, setCurrent] = useState("");
-
-    useEffect(() => {
-        process.browser && setCurrent(window.location.pathname);
-    }, [process.browser && window.location.pathname]);
-    return (
-        <div className="nav flex-column nav-pills mt-2 ">
-            <Link href="/user"  legacyBehavior>
-                <a className={`nav-link ${current === "/user" && "active"}`}>
-                    Dashboard
-                </a>
-            </Link>
-        </div>
-    );
+  useEffect(() => {
+    process.browser && setCurrent(window.location.pathname);
+  }, [process.browser && window.location.pathname]);
+  return (
+    <div className="nav flex-column nav-pills mt-2 ">
+      <Link href="/user" legacyBehavior>
+        <a className={`nav-link ${current === '/user' && 'active'}`}>Dashboard</a>
+      </Link>
+    </div>
+  );
 };
 
 export default UserNav;
--- client/pages/instructor/course/create.js
+++ client/pages/instructor/course/create.js
@@ -1,10 +1,41 @@
-import axios from "axios";
-import InstructorRoute from "../../../component/routes/InstructorRoute";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import InstructorRoute from '../../../component/routes/InstructorRoute';
+import CourseCreateForm from '../../../component/forms/CourseCreateForm';
 
 const CourseCreate = () => {
+  // state
+  const [values, setValues] = useState({
+    name: '',
+    description: '',
+    price: '9.99',
+    uploading: false,
+    paid: true,
+    category: '',
+    loading: false,
+    imagePreview: '',
+  });
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleImage = () => {
+    //
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(values);
+  };
+
   return (
     <InstructorRoute>
-      <h1 className="container-fluid p-5 bg-primary text-white text-center">Create Course</h1>
+      <h1 className="jumbotron text-center square">Create Course</h1>
+      <div className="pt-3 pb-3">
+        <CourseCreateForm handleSubmit={handleSubmit} handleImage={handleImage} handleChange={handleChange} values={values} setValues={setValues} />
+      </div>
+      <pre>{JSON.stringify(values, null, 4)}</pre>
     </InstructorRoute>
   );
 };
